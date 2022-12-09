acad2f8b90e5236ce054d43185404cc3010c0de7
tlangs <tlangs@broadinstitute.org>
2022-12-09 09:39:46 -0500

    Some reflection

--- src/main/java/org/tlangs/Main.java
+++ src/main/java/org/tlangs/Main.java
@@ -1,18 +1,11 @@
 package org.tlangs;
 
 import org.tlangs.question.Question;
-import org.tlangs.question.Question1;
-import org.tlangs.question.Question2;
-import org.tlangs.question.Question3;
-import org.tlangs.question.Question4;
-import org.tlangs.question.Question5;
-import org.tlangs.question.Question6;
-import org.tlangs.question.Question7;
-import org.tlangs.question.Question8;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Objects;
 
 public class Main {
@@ -27,16 +20,13 @@ public class Main {
   }
 
   private static Question getQuestion(String day) {
-    return switch (day.substring(0, 1)) {
-      case "1" -> new Question1();
-      case "2" -> new Question2();
-      case "3" -> new Question3();
-      case "4" -> new Question4();
-      case "5" -> new Question5();
-      case "6" -> new Question6();
-      case "7" -> new Question7();
-      case "8" -> new Question8();
-      default -> throw new RuntimeException(String.format("Invalid day: [%s]", day));
-    };
+    var questionNum = day.substring(0, 1);
+    try {
+      Class<Question> aQuestion = (Class<Question>) Class.forName(String.format("org.tlangs.question.Question%s", questionNum));
+      return aQuestion.getDeclaredConstructor().newInstance();
+    } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException |
+             IllegalAccessException | InvocationTargetException e) {
+      throw new RuntimeException(String.format("Invalid question: [%s]", questionNum), e);
+    }
   }
 }
---
+++ src/main/java/org/tlangs/question/Question9.java
@@ -0,0 +1,11 @@
+package org.tlangs.question;
+
+import java.util.stream.Stream;
+
+public class Question9 implements Question {
+
+  @Override
+  public void answer(Stream<String> lines) {
+
+  }
+}
