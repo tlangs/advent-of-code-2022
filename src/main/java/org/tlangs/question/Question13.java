7fc4e929b71ad8e9cd6e5e33da6f1187f141400f
tlangs <tlangs@broadinstitute.org>
2022-12-13 15:24:04 -0500

    Finally, a day 13 parser

--- src/main/java/org/tlangs/question/Question13.java
+++ src/main/java/org/tlangs/question/Question13.java
@@ -1,52 +1,130 @@
 package org.tlangs.question;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Question13 implements Question {
+  @Override
+  public void answer(Stream<String> lines) {
+    lines.filter(s -> !s.isBlank()).map(s -> NestedList.parseFromString(s, Integer::parseInt)).forEach(System.out::println);
+  }
+
+  static abstract class NestedList<T> {
+    public abstract List<NestedList<T>> values();
+
+    public static <T> NestedList<T> parseFromString(String line, Function<String, T> elementParser) {
+      if (line.startsWith("[")) {
+        return parseList(elementsOfList(line), elementParser);
+      } else {
+        return new SingleValue<>(line, elementParser);
+      }
+    }
+
+    public static <T> ListValue<T> parseList(String line, Function<String, T> elementParser) {
+      ListValue<T> result = new ListValue<>();
+      if (line.isBlank()) {
+        return result;
+      }
+      var head = line;
+      String tail;
+
+      while (head != null) {
+        if (head.startsWith("[")) {
+          var element = parseList(elementsOfList(head), elementParser);
+          result.addElement(element);
+          var indexOfComma = head.indexOf(",", element.toString().length() - 1);
+          if (indexOfComma == -1) {
+            tail = null;
+          } else {
+            tail = head.substring(indexOfComma + 1);
+          }
+        } else {
+          var element = NestedList.parseElement(head.split(",")[0], elementParser);
+          result.addElement(element);
+          var indexOfComma = head.indexOf(",", element.toString().length() - 1);
+          if (indexOfComma == -1) {
+            tail = null;
+          } else {
+            tail = head.substring(indexOfComma + 1);
+          }
+        }
+        head = tail;
+      }
+      return result;
+    }
+
+    public static <T> NestedList<T> parseElement(String element, Function<String, T> elementParser) {
+      if (element.startsWith("[")) {
+        return parseList(element, elementParser);
+      } else {
+        return new SingleValue<>(element, elementParser);
+      }
+    }
+
+    public static String elementsOfList(String string) {
+      if (string.equals("[]")) {
+        return "";
+      }
+      int openings = 0;
+      var array = string.toCharArray();
+      for (int i = 0; i < array.length; i++) {
+        var c = array[i];
+        if (c == ']') {
+          openings--;
+          if (openings == 0) {
+            return string.substring(1, i);
+          }
+        }
+        if (c == '[') {
+          openings++;
+        }
+      }
+      throw new RuntimeException("Could not find closing of " + string);
+    }
+  }
+
+  static class ListValue<T> extends NestedList<T> {
+
+    private List<NestedList<T>> values = new ArrayList<>();
+
+
+    public ListValue() {
+
+    }
+
     @Override
-    public void answer(Stream<String> lines) {
-
+    public List<NestedList<T>> values() {
+      return this.values;
     }
 
-    private Input parseList(String line) {
-
+    public void addElement(NestedList<T> element) {
+      this.values.add(element);
     }
 
-    abstract class Input {
-        public abstract List<Input> values();
+    @Override
+    public String toString() {
+      return "[" + this.values().stream().map(NestedList::toString).collect(Collectors.joining(",")) + "]";
+    }
+  }
+
+  static class SingleValue<T> extends NestedList<T> {
+
+    private T value;
+
+    public SingleValue(String element, Function<String, T> elementParser) {
+      this.value = elementParser.apply(element);
     }
 
-    class SingleValue extends Input {
-
-        private char value;
-        public SingleValue(char value) {
-            this.value = value;
-        }
-
-        public List<Input> values() {
-            return List.of(this);
-        }
-
-        @Override
-        public String toString() {
-            return Character.toString(value);
-        }
+    public List<NestedList<T>> values() {
+      return List.of(this);
     }
 
-    class ListValue extends Input {
-
-        private List<Input> values;
-
-        @Override
-        public List<Input> values() {
-            return this.values;
-        }
-
-        @Override
-        public String toString() {
-            return "[" + this.values().stream().map(Input::toString).collect(Collectors.joining(", ")) + "]";
-        }
+    @Override
+    public String toString() {
+      return value.toString();
     }
+  }
 }
