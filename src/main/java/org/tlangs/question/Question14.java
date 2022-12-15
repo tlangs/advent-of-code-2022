031976b6709f178fb3adfa1d28d14546565c7198
tlangs <tlangs@broadinstitute.org>
2022-12-14 21:37:08 -0500

    Day 14, part 2, slowly

--- src/main/java/org/tlangs/question/Question14.java
+++ src/main/java/org/tlangs/question/Question14.java
@@ -3,9 +3,12 @@ package org.tlangs.question;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.Stack;
 import java.util.function.IntUnaryOperator;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -13,63 +16,246 @@ public class Question14 implements Question {
 
   @Override
   public void answer(Stream<String> lines) {
-    var list = lines.map(Path::new).toList();
-    vizualize(list);
+    var rockPaths = lines.map(Path::new).toList();
+    var cave = new Cave(rockPaths);
+    var grainsOfSand = cave.fill();
+//    cave.visualize();
+    System.out.printf("[%d] grains of sand come to rest before sand starts flowing into the abyss below%n", grainsOfSand);
+
+    var rockPathsWithFloor = addFloor(rockPaths);
+    var caveWithFloor = new Cave(rockPathsWithFloor);
+    var grainsOfSandWithFloor = caveWithFloor.fill();
+//    caveWithFloor.visualize();
+    System.out.printf("With a floor in the cave, [%d] grains of sand come to rest%n", grainsOfSandWithFloor);
   }
 
+  private List<Path> addFloor(List<Path> rockPaths) {
+    var minX = minX(rockPaths);
+    var maxX = maxX(rockPaths);
+    var maxY = maxY(rockPaths);
+    var floorY = maxY + 2;
+    var floorMinX = minX - floorY;
+    var floorMaxX = maxX + floorY;
+    var pathString = String.format("%s,%s -> %s,%s", floorMinX, floorY, floorMaxX, floorY);
+    var floor = new Path(pathString);
+    var newPaths = new ArrayList<>(rockPaths);
+    newPaths.add(floor);
+    return newPaths;
+  }
 
-  private void vizualize(List<Path> paths) {
-    var minX = paths.stream().mapToInt(Path::minX).min().orElseThrow();
-    var maxX = paths.stream().mapToInt(Path::maxX).max().orElseThrow();
-    var maxY = paths.stream().mapToInt(Path::maxY).max().orElseThrow();
-    var array = new Character[maxY + 1][maxX - minX + 1];
+  private int minX(List<Path> rockPaths) {
+    return rockPaths.stream().mapToInt(Path::minX).min().orElseThrow();
+  }
 
-    for (int y = 0; y <= maxY; y++) {
-      var row = array[y];
-      for (int x = minX; x <= maxX; x++) {
-        array[y][x - minX] = '.';
-      }
+  private int maxX(List<Path> rockPaths) {
+    return rockPaths.stream().mapToInt(Path::maxX).max().orElseThrow();
+  }
+
+  private int maxY(List<Path> rockPaths) {
+    return rockPaths.stream().mapToInt(Path::maxY).max().orElseThrow();
+  }
+
+  class Cave {
+
+    public static Position sandStartingPosition = new Position(500, 0);
+    private final List<Path> rockPaths = new ArrayList<>();
+    private final List<SandParticle> sandParticles = new ArrayList<>();
+    private final Set<Position> allPositions = new HashSet<>();
+    public final int minX;
+    public final int maxX;
+    public final int maxY;
+
+    public Cave(List<Path> rockPaths) {
+
+      this.rockPaths.addAll(rockPaths);
+
+      this.allPositions.addAll(rockPaths.stream().flatMap(p -> p.positions.stream()).toList());
+
+      this.minX = minX(rockPaths);
+      this.maxX = maxX(rockPaths);
+      this.maxY = maxY(rockPaths);
     }
 
-    paths.forEach(p -> p.draw(array, minX));
+    private int fill() {
+      int i = 0;
+      var sandParticle = new SandParticle(sandStartingPosition);
+      while (!sandParticle.willFall(Stream.concat(allPositions.stream(), sandParticles.stream().map(s -> s.position)).collect(Collectors.toSet()), maxY)) {
+        allPositions.add(sandParticle.position);
+        sandParticles.add(sandParticle);
+        sandParticle = new SandParticle(sandStartingPosition);
+        if (isFull()) {
+          allPositions.add(sandParticle.position);
+          sandParticles.add(sandParticle);
+          return sandParticles.size();
+        }
+        if (i % 100 == 0) {
+          pruneAllPositions();
+        }
+        i++;
+      }
+      return this.sandParticles.size();
+    }
 
-    array[0][500 - minX] = '+';
-    printGrid(array, minX, maxX);
-  }
+    private void pruneAllPositions() {
+      var rockPathPositions = rockPaths.stream().flatMap(p -> p.positions.stream()).collect(Collectors.toSet());
+      for (var sandParticle : sandParticles) {
+        var thisPosition = sandParticle.position;
+        var above = new Position(thisPosition.x, thisPosition.y - 1);
+        if (allPositions.contains(above) && !rockPathPositions.contains(above)) {
+          allPositions.remove(thisPosition);
+        }
+      }
+      System.out.println(allPositions.size());
+    }
 
-  private void printGrid(Character[][] grid, int minX, int maxX) {
+    private boolean isFull() {
+      return !new SandParticle(sandStartingPosition).canMove(allPositions);
+    }
 
-    var offset = Integer.toString(grid.length).length() + 1;
-    // Labels must be 3-digit integers
-    for (int y = 0; y < 3; y++) {
-      for (int x = 0; x < grid[0].length + offset; x++) {
-        var printedLabel = false;
-        for (int label : List.of(minX, maxX, 500)) {
-          if (x - offset == label - minX) {
-            var labelString = Integer.toString(label);
-            System.out.printf(labelString.substring(y, y + 1));
-            printedLabel = true;
+    private void visualize() {
+
+      var array = new Character[maxY + 1][maxX - minX + 1];
+
+      for (int y = 0; y <= maxY; y++) {
+        var row = array[y];
+        for (int x = minX; x <= maxX; x++) {
+          array[y][x - minX] = '.';
+        }
+      }
+
+      rockPaths.forEach(p -> p.draw(array, minX));
+      sandParticles.forEach(s -> s.draw(array, minX));
+
+      array[0][500 - minX] = '+';
+      printGrid(array, minX, maxX);
+    }
+
+    private void printGrid(Character[][] grid, int minX, int maxX) {
+
+      var offset = Integer.toString(grid.length).length() + 1;
+      // Labels must be 3-digit integers
+      for (int y = 0; y < 3; y++) {
+        for (int x = 0; x < grid[0].length + offset; x++) {
+          var printedLabel = false;
+          for (int label : List.of(minX, maxX, 500)) {
+            if (x - offset == label - minX) {
+              var labelString = Integer.toString(label);
+              System.out.printf(labelString.substring(y, y + 1));
+              printedLabel = true;
+            }
+          }
+          if (!printedLabel) {
+            System.out.printf(" ");
           }
         }
-        if (!printedLabel) {
-          System.out.printf(" ");
-        }
+        System.out.println();
       }
-      System.out.println();
+
+
+      for (int y = 0; y < grid.length; y++) {
+        var row = grid[y];
+        var yLabel = Integer.toString(y);
+        while (yLabel.length() < offset) {
+          yLabel = yLabel + " ";
+        }
+        System.out.printf("%s", yLabel);
+        for (int x = 0; x < row.length; x++) {
+          System.out.printf(row[x].toString());
+        }
+        System.out.println();
+      }
+    }
+  }
+
+  static class SandParticle {
+
+    private Position position;
+
+    public SandParticle(Position position) {
+      this.position = position;
     }
 
+    public boolean willFall(Set<Position> allOtherPositions, int maxY) {
+      while (canMove(allOtherPositions)) {
+        move(allOtherPositions);
+        if (position.y > maxY) {
+          return true;
+        }
+      }
+      return false;
+    }
 
-    for (int y = 0; y < grid.length; y++) {
-      var row = grid[y];
-      var yLabel = Integer.toString(y);
-      while (yLabel.length() < offset) {
-        yLabel = yLabel + " ";
+    public boolean canMove(Set<Position> allPositions) {
+      var positionsBelow = positionsBelow(allPositions);
+      return canMoveDown(positionsBelow) ||
+          canMoveDiagonalLeft(positionsBelow) ||
+          canMoveDiagonalRight(positionsBelow);
+    }
+
+    public boolean move(Set<Position> allPositions) {
+      var positionsBelow = positionsBelow(allPositions);
+      var canMoveDown = canMoveDown(positionsBelow);
+      if (canMoveDown) {
+        moveAllTheWayDown(allPositions);
+        return true;
       }
-      System.out.printf("%s", yLabel);
-      for (int x = 0; x < row.length; x++) {
-        System.out.printf(row[x].toString());
+      var canMoveDiagonalLeft = canMoveDiagonalLeft(positionsBelow);
+      if (canMoveDiagonalLeft) {
+        this.position = new Position(position.x - 1, position.y + 1);
+        return true;
       }
-      System.out.println();
+      var canMoveDiagonalRight = canMoveDiagonalRight(positionsBelow);
+      if (canMoveDiagonalRight) {
+        this.position = new Position(position.x + 1, position.y + 1);
+        return true;
+      }
+      return false;
+    }
+
+    private List<Position> positionsBelow(Set<Position> allPositions) {
+      return allPositions.stream()
+          .filter(p -> p.x() >= position.x - 1 && p.x() <= position.x + 1)
+          .filter(p -> p.y() == position.y + 1)
+          .toList();
+    }
+
+    private void moveAllTheWayDown(Set<Position> allPositions) {
+      var everythingBelow  = allPositions.stream()
+          .filter(p -> p.x == position.x)
+          .filter(p -> p.y > position.y)
+          .min(Comparator.comparing(Position::y));
+      var yValue = everythingBelow.map(Position::y).map(i -> i - 1).orElse(position.y + 1);
+      this.position = new Position(position.x, yValue);
+    }
+
+    private boolean canMoveDown(List<Position> positionsBelow) {
+      return positionsBelow.stream()
+          .filter(p -> p.x() == position.x)
+          .filter(p -> p.y() == position.y + 1)
+          .findFirst()
+          .isEmpty();
+    }
+
+    private boolean canMoveDiagonalLeft(List<Position> positionsBelow) {
+      return positionsBelow.stream()
+          .filter(p -> p.x() == position.x - 1)
+          .filter(p -> p.y() == position.y + 1)
+          .findFirst()
+          .isEmpty();
+    }
+
+    private boolean canMoveDiagonalRight(List<Position> positionsBelow) {
+      return positionsBelow.stream()
+          .filter(p -> p.x() == position.x + 1)
+          .filter(p -> p.y() == position.y + 1)
+          .findFirst()
+          .isEmpty();
+    }
+
+    public void draw(Character[][] array, int gridMinX) {
+      var y = position.y;
+      var x = position.x - gridMinX;
+      array[y][x] = 'o';
     }
   }
 
@@ -77,6 +263,7 @@ public class Question14 implements Question {
   class Path {
 
     private List<Position> positions;
+
     public Path(String line) {
       this.positions = getPositionsFromVertices(parseVertices(line));
     }
@@ -113,7 +300,7 @@ public class Question14 implements Question {
     private List<Integer[]> parseVertices(String line) {
       return Arrays.stream(line.split(" -> "))
           .map(s -> s.split(","))
-          .map(a -> new Integer[] {Integer.parseInt(a[0]), Integer.parseInt(a[1])})
+          .map(a -> new Integer[]{Integer.parseInt(a[0]), Integer.parseInt(a[1])})
           .toList();
     }
 
@@ -168,7 +355,7 @@ public class Question14 implements Question {
     }
   }
 
+  record Position(int x, int y) {
+  }
 
-
-  record Position(int x, int y) {}
 }
